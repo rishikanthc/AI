25850a7adaf44ccd6b7c3c641e766b73a756e2e3
rishikanthc <rishikanthc@gmail.com>
2016-06-20 01:20:06 -0400

    final edits

--- 2048/AlphaBeta-3/BaseAI.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-#coding:utf-8
-
-class BaseAI:
-	def getMove(self, grid):
-		pass
--- 2048/AlphaBeta-3/BaseDisplayer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#coding:utf-8
-
-class BaseDisplayer:
-	def __init__(self):
-		pass
-
-	def display(self, grid):
-		pass
